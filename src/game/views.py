a83ddd7bcd8b8327c242481615aecc402eba1c17
Gonzo <agonzalez1043@gmail.com>
2025-05-15 21:16:12 -0700

    Adding to wishlist now gives confirmation message and stays on search page

--- src/game/views.py
+++ src/game/views.py
@@ -6,17 +6,29 @@ from .models import Game, Genre, Theme, Platform
 # Create your views here.
 def search_view(request):
     context = {}
-    if request.method == 'POST': # Search has been initiated
-        # extract info from POST
-        title = request.POST['title']
-        # search for title
-        results = Game.title_search(title)
-        # show results that come back
-        if results:
-            context = {'games': results,
-                       'searched_title':title}
-        else:
-            context = {'no_results':True}
+    user = request.user
+    if request.method == "POST":
+        if 'game_id' in request.POST: # Search is complete add game to wishlist
+            if user.is_authenticated:
+                game_id = request.POST['game_id']
+                game = Game.add_or_grab_game(game_id)
+                user.patron.wishlist.add(game)
+                context = {'confirm_message': (game.title + ' has been added to your wishlist!')}
+            else:
+                message = 'Must be signed in add games to a wishlist.'
+                context = {'error_message': message}  # change this to go to an error page
+                return render(request, 'error.html', context)
+        else: # Search has been initiated
+            # extract info from POST
+            title = request.POST['title']
+            # search for title
+            results = Game.title_search(title)
+            # show results that come back
+            if results:
+                context = {'games': results,
+                           'searched_title':title}
+            else:
+                context = {'no_results':True}
 
     return render(request,'game/search_game.html',context)
 
