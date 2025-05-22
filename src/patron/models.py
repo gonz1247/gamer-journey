92ab725bf9f60632feca877060d98bd0aa04e69b
Gonzo <agonzalez1043@gmail.com>
2025-05-21 21:02:16 -0700

    gave myself (superuser) a patron account

--- src/patron/models.py
+++ src/patron/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from game.models import Game
 # Create your models here.
 
-# TODO: Give myself (superuser) a patron account manually
 class Patron(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     wishlist = models.ManyToManyField(Game)
