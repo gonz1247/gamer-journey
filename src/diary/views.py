5979d46d7ea067f74e6d29f7b26c4ad1bfc3122a
Gonzo <agonzalez1043@gmail.com>
2025-06-01 17:20:22 -0700

    update Game model to use game_id as primary key so that can use pk in a diary many to many relationship to query info on games rather than store in the Game or DiaryEntry models

--- src/diary/views.py
+++ src/diary/views.py
@@ -13,7 +13,8 @@ def diary_entry_view(request):
             form = DiaryEntryForm(request.POST)
             # add in choice fields again since this is generated dynamically when first loading the diary add page
             game = Game.add_or_grab_game(request.POST['game']) # potential security concern since they can change the game they are adding midway (by changing HTML), but then it just adds a different game so not a big deal
-            platforms = {p.device: p.device for p in game.platforms.all()}
+            [game_info] = Game.game_id_search(game.game_id, fields='platforms.*')
+            platforms = {p: p for p in game_info['platforms']}
             form.fields['platform'].choices = platforms
             if form.is_valid():
                 form.save()
@@ -31,16 +32,16 @@ def diary_entry_view(request):
         elif request.method == 'POST' and 'game_id' in request.POST:
             # grab game info to display on the diary entry page
             game_id = request.POST['game_id']
-            game = Game.add_or_grab_game(game_id)
+            [game_info] = Game.game_id_search(game_id, fields='name,cover.*,platforms.*')
             # look up what platforms the game is available on and add options to form
-            platforms = {p.device:p.device for p in game.platforms.all()}
+            platforms = {p:p for p in game_info['platforms']}
             form = DiaryEntryForm(initial={'game':game_id, 'patron':user.patron.pk})
             form.fields['platform'].choices = platforms
             if user.patron.fav_platform in platforms:
                 form.initial['platform'] = user.patron.fav_platform
             context = {
                 'form':form,
-                'game':game,
+                'game':game_info,
             }
             return render(request, 'diary/diary_entry.html', context)
         else:
@@ -54,7 +55,14 @@ def my_diary_view(request):
     user = request.user
     if user.is_authenticated:
         current_diary = user.patron.diaryentry_set.all()
-        current_diary_sorted = sorted(current_diary, key=lambda entry: entry.completed_date)
+        # Get game info for each entry
+        game_info = Game.game_id_search(current_diary.values_list('game_id',flat=True),fields='name,cover.*')
+        # Convert entry and game info to dictionary so that game_info can be matched up with entry (API returns info in order of game_id, not order or request list)
+        current_diary_sorted = list()
+        for info in game_info:
+            current_diary_sorted.append(current_diary.get(game_id=info['game_id']).__dict__ | info)
+        # sort by completion date
+        current_diary_sorted = sorted(current_diary_sorted, key=lambda entry: entry['completed_date'])
         context = {'diary': current_diary_sorted}
         return render(request, 'diary/my_diary.html', context)
     else:
@@ -68,28 +76,28 @@ def diary_edit_view(request, entry_id):
         try:
             # grab diary entry
             entry = user.patron.diaryentry_set.get(pk=entry_id)
-            game = entry.game
+            [game_info] = Game.game_id_search(entry.game.game_id, fields='name,cover.*,platforms.*')
         except DiaryEntry.DoesNotExist:
             message = "Cannot edit another patron's diary entry."
             context = {'error_message': message}
             return render(request, 'error.html', context)
-        if 'entry_id' in request.POST or request.method == 'GET': # user to manually type out URL is they really want
+        if 'entry_id' in request.POST or request.method == 'GET': # user to manually type out URL if they really want
             form = DiaryEntryForm(instance=entry)
             # update platform choices that were done dynamically the first time
-            platforms = {p.device: p.device for p in game.platforms.all()}
+            platforms = {p: p for p in game_info['platforms']}
             form.fields['platform'].choices = platforms
             # Add in intial data for fields that had their data structure type changed in cleaning
             form.initial['platform'] = entry.platform.device
             form.initial['game'] = entry.game.game_id
             context = {
                 'form':form,
-                'game':game,
+                'game':game_info,
             }
             return render(request, 'diary/diary_entry.html', context)
         elif request.method == 'POST': # entry has been updated if post, if get then just manually trying to come to page
             form = DiaryEntryForm(request.POST, instance=entry)
             # update platform choices that were done dynamically the first time
-            platforms = {p.device: p.device for p in game.platforms.all()}
+            platforms = {p: p for p in game_info['platforms']}
             form.fields['platform'].choices = platforms
             if form.is_valid():
                 form.save()
@@ -106,7 +114,7 @@ def diary_delete_view(request, entry_id):
         try:
             # grab diary entry
             entry = user.patron.diaryentry_set.get(pk=entry_id)
-            game = entry.game
+            [game_info] = Game.game_id_search(entry.game.game_id, fields='name,cover.*')
         except DiaryEntry.DoesNotExist:
             message = "Cannot delete another patron's diary entry."
             context = {'error_message': message}
@@ -117,15 +125,17 @@ def diary_delete_view(request, entry_id):
             return redirect('diary')
         else:
             context = {
-                'game':game,
+                'game':game_info,
             }
             return render(request, 'diary/delete_confirmation.html', context)
 
 def diary_detailed_view(request, entry_id):
     # grab diary entry, don't really care if it's that users or not
     entry = get_object_or_404(DiaryEntry.objects.all(),pk=entry_id)
+    [game_info] = Game.game_id_search(entry.game.game_id, fields='name,cover.*')
     context = {
         'entry':entry,
+        'game':game_info,
     }
     return render(request, 'diary/view_detailed.html', context)
 
@@ -135,6 +145,9 @@ def game_reviews_view(request, game_id):
         game = Game.objects.get(game_id=game_id)
         entries = game.diaryentry_set.all()
         assert len(entries)>0
+        # Get game info
+        [game_info] = game.self_search(fields='name,cover.*')
+        # Generate review stats
         total = 0
         n_reviews = 0
         for entry in entries:
@@ -146,7 +159,7 @@ def game_reviews_view(request, game_id):
         # Can add more stats later if I want
         context = {
             'entries': entries,
-            'game':game,
+            'game':game_info,
             'review_stats':review_stats,
         }
         return render(request, 'diary/view_all_reviews.html', context)
